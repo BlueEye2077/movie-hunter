5a2df621b4b4590b88641621dc91eeab046f7d9f
BlueEye2077 <ab3062004@gmail.com>
2026-04-18 23:17:50 +0200

    feat(network): add tmdb images base url and secure api key

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String apiKey =
-      '';
+  static const String imagesUrl = 'https://image.tmdb.org/t/p/w500';
+  static const String apiKey = '';
 }
