c1ca3e82384dfa1ce9b2fc258deb979a946c5559
BlueEye2077 <ab3062004@gmail.com>
2026-04-24 03:04:06 +0300

    chore: update routing for search feature

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_hunter/core/di/dependency_injection.dart';
+import 'package:movie_hunter/features/home/logic/cubit/genres_cubit.dart';
+import 'package:movie_hunter/features/search/logic/cubit/search_cubit.dart';
 import 'package:movie_hunter/main_screen.dart';
 import 'package:movie_hunter/features/onboarding/ui/screens/onboarding_screen.dart';
 import 'package:movie_hunter/features/search/ui/screens/search_screen.dart';
@@ -13,7 +17,19 @@ class AppRouter {
         // BlocProviders are now scoped inside MainScreen per-tab (Option B)
         return MaterialPageRoute(builder: (_) => const MainScreen());
       case Routes.search:
-        return MaterialPageRoute(builder: (_) => const SearchScreen());
+        return MaterialPageRoute(
+          builder: (_) => MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) => SearchCubit(searchRepository: getIt()),
+              ),
+              BlocProvider.value(
+                value: getIt<GenresCubit>(),
+              ),
+            ],
+            child: const SearchScreen(),
+          ),
+        );
       default:
         return null;
     }
